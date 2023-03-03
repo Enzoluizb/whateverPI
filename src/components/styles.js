a818796d5b5e67e95541aadb81df846862b65c49
Enzoluizb <enzoluiz.brt@gmail.com>
2023-03-03 17:00:45 -0300

    HistoricoScreen

--- src/components/styles.js
+++ src/components/styles.js
@@ -7,7 +7,7 @@ export const styles = StyleSheet.create({
     justifyContent: "flex-start",
     alignItems: "center",
     backgroundColor: "#2D4B73",
-    gap: 15
+    gap: 15,
   },
   Logo: {
     fontFamily: "Red Rose",
@@ -29,70 +29,70 @@ export const styles = StyleSheet.create({
     shadowRadius: 5,
   },
   button: {
-    backgroundColor: '#D9B70D',
+    backgroundColor: "#D9B70D",
     height: 34,
     width: 222,
-    justifyContent: 'center',
-    alignItems: 'center',
+    justifyContent: "center",
+    alignItems: "center",
     borderRadius: 4,
   },
   label: {
     fontSize: 14,
-    color: '#2D4B73',
-    fontWeight: 'bold',
-    textTransform: 'uppercase',
+    color: "#2D4B73",
+    fontWeight: "bold",
+    textTransform: "uppercase",
   },
   TextoEsqueceuSenha: {
-     color: '#D9B70D',
-     fontWeight: 600,
+    color: "#D9B70D",
+    fontWeight: 600,
   },
   TextoNaoTemUmaConta1: {
-    color: 'white',
+    color: "white",
     fontWeight: 600,
     textAlign: "center",
   },
   TextoNaoTemUmaConta2: {
-    color: '#D9B70D',
+    color: "#D9B70D",
     fontWeight: 600,
     textAlign: "center",
     border: "2px solid #D9B70D",
     borderRadius: "12px",
     padding: 7,
-    marginTop: 10
+    marginTop: 10,
   },
   linha: {
-    width: '334px',
-    height: '0px',
-    left: '42px',
-    border: '1px solid #C1C1C1'
+    width: "334px",
+    height: "0px",
+    left: "42px",
+    border: "1px solid #C1C1C1",
   },
   textoPrimario: {
-    color: 'white',
+    color: "white",
     fontSize: 15,
-    fontWeight: 500
+    fontWeight: 500,
   },
   textoPrimarioCad: {
-    color: 'white',
+    color: "white",
     fontSize: 25,
-    fontWeight: 500
+    fontWeight: 500,
   },
   TextosRegistro: {
     gap: 30,
-    color: 'white',
+    color: "white",
     fontSize: 18,
     fontWeight: 500,
-    flex:1,
+    flex: 1,
     flexDirection: "column",
-    alignItems: "center"
+    alignItems: "center",
   },
   TextosRegistro2: {
     gap: 30,
-    color: '#D9B70D',
+    color: "#D9B70D",
     fontSize: 18,
     fontWeight: 500,
-    flex:1,
+    flex: 1,
     flexDirection: "column",
-    alignItems: "center"
+    alignItems: "center",
   },
 
   // style recuperar senha
@@ -129,34 +129,31 @@ export const styles = StyleSheet.create({
   },
   //style do perfil
   perfil: {
-    color:"white",
-    backgroundColor:"#2D4B73",
-    flex:1,
-    alignItems:"center",
-    gap:89,
+    color: "white",
+    backgroundColor: "#2D4B73",
+    flex: 1,
+    alignItems: "center",
+    gap: 89,
   },
-  perfil2:{
-    color:"white",
-    fontFamily:"Red Rose",
-    fontSize:23
-
+  perfil2: {
+    color: "white",
+    fontFamily: "Red Rose",
+    fontSize: 23,
   },
   buttonSalvar: {
-    backgroundColor: '#ECECEC',
+    backgroundColor: "#ECECEC",
     height: 34,
     width: 222,
-    justifyContent: 'center',
-    alignItems: 'center',
+    justifyContent: "center",
+    alignItems: "center",
     borderRadius: 4,
-    color:"black",
-  
+    color: "black",
   },
   labelSalvar: {
     fontSize: 14,
-    color: '#2D4B73',
-    fontWeight: 'bold',
-    textTransform: 'uppercase',
-    
+    color: "#2D4B73",
+    fontWeight: "bold",
+    textTransform: "uppercase",
   },
   inputSalvar: {
     height: 34,
@@ -171,26 +168,73 @@ export const styles = StyleSheet.create({
     shadowOpacity: 0.2,
     shadowRadius: 5,
   },
-  
+
   //foto perfil
   imgPerfil: {
     width: 80,
     height: 80,
     borderRadius: 90,
-    marginLeft: 65
+    marginLeft: 65,
   },
   FotoTexto: {
     gap: 10,
-    backgroundColor:"#ECECEC",
+    backgroundColor: "#ECECEC",
     maxHeight: "500px",
-    flex: 0.30,
+    flex: 0.3,
     flexDirection: "row",
     width: "400px",
     alignItems: "center",
   },
-  Perfilnome:{
-    fontFamily:"Red Rose",
-    color:"black",
-    fontSize: 22
+  Perfilnome: {
+    fontFamily: "Red Rose",
+    color: "black",
+    fontSize: 22,
+  },
+  containerHistorico: {
+    backgroundColor: "#2D4B73",
+    flex: 1,
+    gap: 25,
+  },
+  historico: {
+    color: "white",
+    backgroundColor: "#ECECEC",
+    flex: 1,
+    gap: 20,
+    padding:20,
+    flexDirection: 'row',
+    alignItems: 'flex-start'
+  },
+  historicoTest: {
+    color: "white",
+    backgroundColor: "#ECECEC",
+    flex: 1,
+    gap: 20,
+    padding:20,
+    flexDirection: 'column',
+     alignItems: 'flex-start'
+  },
+  textoHistorico: {
+    color: "white",
+    textAlign: "center",
+    fontWeight: "bold",
+    textTransform: "uppercase",
+    fontSize: 20,
+    fontFamily: "Red Rose",
+  },
+  gap:{
+    gap:20
+  },
+  linhaH: {
+    width: "434px",
+    height: "0px",
+    left: "42px",
+    border: "1px solid #C1C1C1",
+  },
+  textoH: {
+    color: "black",
+    fontWeight: "bold",
+    textTransform: "uppercase",
+    fontSize: 13,
+    
   }
 });
--- src/screens/HistoricoScreen.jsx
+++ src/screens/HistoricoScreen.jsx
@@ -1,12 +1,107 @@
 import { View } from "react-native";
 import { Text } from "react-native-paper";
 import { styles } from "../components/styles";
+import { CheckBox } from "react-native-elements";
+import { useState } from "react";
 
 export default function HistoricoScreen() {
+  const [isChecked, setIsChecked] = useState(false);
+
   return (
-    <View style={styles.container}>
-      <Text>Histórico</Text>
+    <View style={styles.containerHistorico}>
+      <Text style={styles.textoHistorico}>Histórico</Text>
+      <View>
+        <View style={styles.gap}>
+          <View style={styles.historico}>
+            <View style={styles.historicoTest}>
+            <Text style={styles.textoH}>Restaurante encontrados!</Text>
+            <Text style={styles.textoH}>Dia 20/01/2023</Text>
+            </View>
+            <CheckBox
+              title=""
+              checked={isChecked}
+              onPress={() => setIsChecked(!isChecked)}
+            />
+          </View>
+        </View>
+        <Text style={styles.linhaH} />
+        <View style={styles.historico}>
+        <View style={styles.historicoTest}>
+          <Text style={styles.textoH}>Eventos esportivos encontrados!</Text>
+          <Text style={styles.textoH}>Dia 18/01/2023</Text>
+          </View>
+          <CheckBox
+              title=""
+              checked={isChecked}
+              onPress={() => setIsChecked(!isChecked)}
+            />
+        </View>
+        <Text style={styles.linhaH} />
+        <View style={styles.historico}>
+        <View style={styles.historicoTest}>
+          <Text style={styles.textoH}>Festas encontrados!</Text>
+          <Text style={styles.textoH}>Dia 20/12/2023</Text>
+          </View>
+          <CheckBox
+              title=""
+              checked={isChecked}
+              onPress={() => setIsChecked(!isChecked)}
+            />
+        </View>
+        <Text style={styles.linhaH} />
+        <View style={styles.historico}>
+        <View style={styles.historicoTest}>
+          <Text style={styles.textoH}>Parques encontrados!</Text>
+          <Text style={styles.textoH}>Dia 17/12/2023</Text>
+          </View>
+          <CheckBox
+              title=""
+              checked={isChecked}
+              onPress={() => setIsChecked(!isChecked)}
+            />
+        </View>
+        <Text style={styles.linhaH} />
+        <View style={styles.historico}>
+        <View style={styles.historicoTest}>
+          <Text style={styles.textoH}>Encontros esportivos não encontrados!</Text>
+          <Text style={styles.textoH}>Dia 18/01/2023</Text>
+          </View>
+          <CheckBox
+              title=""
+              checked={isChecked}
+              onPress={() => setIsChecked(!isChecked)}
+            />
+        </View>
+        <Text style={styles.linhaH} />
+        <View style={styles.gap}>
+          <View style={styles.historico}>
+          <View style={styles.historicoTest}>
+            <Text style={styles.textoH}>Restaurante encontrados!</Text>
+            <Text style={styles.textoH}>Dia 20/01/2023</Text>
+            </View>
+            <CheckBox
+              title=""
+              checked={isChecked}
+              onPress={() => setIsChecked(!isChecked)}
+            />
+          </View>
+        </View>
+        <Text style={styles.linhaH} />
+        <View style={styles.gap}>
+          <View style={styles.historico}>
+          <View style={styles.historicoTest}>
+            <Text style={styles.textoH}>Restaurante encontrados!</Text>
+            <Text style={styles.textoH}>Dia 20/01/2023</Text>
+            </View>
+            <CheckBox
+              title=""
+              checked={isChecked}
+              onPress={() => setIsChecked(!isChecked)}
+            />
+          </View>
+        </View>
+        <Text style={styles.linhaH} />
+      </View>
     </View>
-    
   );
 }
